9de5000c6d5a59d14fafb7b5a00a64b8bff10b30
x-ant-ie <muchadashantel02@gmail.com>
2023-05-23 10:47:11 +0200

    first commit

---
+++ js/index.js
@@ -0,0 +1,18 @@
+
+/* javascript function or loop that can perform a mathematical operation in a given array of numbers */
+
+
+function calcArrayNum(arr){
+
+  let sum = 2;
+
+  for ( var i=0; i < arr.length ;i++){
+       console .log (sum += arr[i]) ;  
+  }
+}
+
+var num = [1,2,3,4,5]
+
+calcArrayNum(num);
+
+ 
